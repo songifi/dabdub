99d5a4332aaf6270a27e96aa50b6bb987e088a3e
Ikem Peter <ikempeter2020@gmail.com>
2026-01-28 09:00:03 +0100

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,20 +6,16 @@ import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // Config & Core Modules
-import { GlobalConfigModule } from './global-config/global-config.module';
-import { GlobalConfigService } from './global-config/global-config.service';
+import { GlobalConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { CacheModule } from './cache/cache.module';
 import { LoggerModule } from './logger/logger.module';
 // Feature Modules
-import { NotificationModule } from './notification/notification.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { SettlementModule } from './settlement/settlement.module';
 import { TransactionsModule } from './transactions/transactions.module';
-import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
-import { SwaggerDocModule } from './swagger-doc/swagger-doc.module';
 import { NotificationModule } from './notification/notification.module';
 import { GlobalConfigService } from './config/global-config.service';
 import { BlockchainModule } from './blockchain/blockchain.module';
@@ -56,12 +52,11 @@ import { EVMModule } from './evm/evm.module';
     NotificationModule,
     AnalyticsModule,
     SettlementModule,
-    TransactionsModule, // Added from feat/transaction-controller-33
+    TransactionsModule,
     BlockchainModule,
     AuthModule,
     HealthModule,
     WebhooksModule,
-    SwaggerDocModule,
     PublicModule,
     EVMModule,
   ],
